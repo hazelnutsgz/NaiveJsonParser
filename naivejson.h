455d82fa36585dec4fb61fdf488325fa10888d68
hazelnutsgz <15201752137@163.com>
2018-06-27 16:38:32 +0800

    Add the status enumerator and value enumerator

--- naivejson.h
+++ naivejson.h
@@ -1,6 +1,33 @@
-#ifndef JSON_PARSER_LIBRARY_H
-#define JSON_PARSER_LIBRARY_H
+#ifndef NAIVEJSON_H__
+#define NAIVEJSON_H__
 
-void hello(void);
+typedef enum {
+    NAIVE_NULL,
+    NAIVE_FALSE,
+    NAIVE_TRUE,
+    NAIVE_NUMBER,
+    NAIVE_STRING,
+    NAIVE_ARRAY,
+    NAIVE_OBJECT
+} naive_type;
+
+typedef struct{
+    naive_type type;
+} naive_value;
+
+enum {
+    NAIVE_PARSE_OK = 0, //NORMAL
+    NAIVE_PARSE_EXPECT_VALUE, //If the json only contains vacuum
+    NAIVE_PARSE_INVALID_VALUE,
+    NAIVE_PARSE_ROOT_NOT_SINGULAR //
+};
+
+
+
+//The parser should not modify the original input string, so we use const identifier
+//The return value is the state of parse.
+int naive_parse(naive_value* v, const char* json);
+
+
 
 #endif
